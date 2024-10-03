5a6b8288a96ccf87d4b8cfe510dfe053e3585753
Quentin Alpesse <qalpesse@fu-r1-p2.s19.be>
2024-10-03 17:52:42 +0200

    update

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qalpesse <qalpesse@student.s19.be>         +#+  +:+       +#+        */
+/*   By: qalpesse <qalpesse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 14:41:39 by qalpesse          #+#    #+#             */
-/*   Updated: 2024/09/24 19:34:27 by qalpesse         ###   ########.fr       */
+/*   Updated: 2024/10/03 17:38:22 by qalpesse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,18 +54,14 @@ char *ft_get_token(char *s, int *i, int *d_quote, int *s_quote)
 	char	*data;
 
 	j = 0;
-	printf("new token\n");
 	*d_quote = 0;
 	*s_quote = 0;
 	while (ft_update_quotes_status(s[j], d_quote, s_quote), s[j] && !ft_is_sp(s[j], d_quote, s_quote) )
-	{
 		j++;
-	}
 	data = malloc((j + 1) * sizeof(char));
 	j = 0;
 	*d_quote = 0;
 	*s_quote = 0;
-	printf("add token in data\n");
 	while (ft_update_quotes_status(s[j], d_quote, s_quote), s[j] && !ft_is_sp(s[j], d_quote, s_quote))
 	{
 		data[j] = s[j];
@@ -75,14 +71,32 @@ char *ft_get_token(char *s, int *i, int *d_quote, int *s_quote)
 	*i += j - 1;
 	return (data);
 }
-char *ft_get_sp_token(char c)
+/*need to handle fouble char as one like >> or &&*/
+char *ft_get_sp_token(char *c, int *i)
 {
 	char *data;
 
+	if (*c == *(c + 1))
+	{
+		data = malloc(3 * sizeof(char));
+		data[0] = *c;
+		data[1] = *(c + 1);
+		data[2] = '\0';
+		*i += 1;
+		return (data);
+	}
 	data = malloc(2 * sizeof(char));
-	data[0] = c;
+	data[0] = *c;
 	data[1] = '\0';
 	return (data);
+}
+void	ft_token_type(t_list *tokens)
+{
+	while (tokens)
+	{
+		if ()
+		tokens = tokens->next
+	}	
 }
 /*king of split but that work directly with chain list and that keep sep if it has to */
 t_list *ft_lexer(char *str)
@@ -99,17 +113,19 @@ t_list *ft_lexer(char *str)
 	while (str[i])
 	{
 		if (!ft_is_sp(str[i], &d_quote_status, &s_quote_status))
-		{
 			ft_lstadd_back(&tokens, ft_lstnew(ft_get_token(&str[i], &i, &d_quote_status, &s_quote_status), WORD));
-		}
 		else
 		{
 			if (ft_is_sp(str[i], &d_quote_status, &s_quote_status) == 1)
-				ft_lstadd_back(&tokens, ft_lstnew(ft_get_sp_token(str[i]), WORD));
+				ft_lstadd_back(&tokens, ft_lstnew(ft_get_sp_token(&str[i], &i), WORD));
 		}
 		i++;
 	}
 	if (d_quote_status || s_quote_status)
+	{
+		ft_lstclear(&tokens,&free); 
 		ft_error("quote not close");
+	}
+	ft_token_type(tokens);
 	return (tokens);
 }
