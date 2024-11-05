179259d86596d36ee955b8f7b938954e207d6212
margauuuuuxx <margaux.loncour@gmail.com>
2024-11-05 12:11:02 +0100

    cd update

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: marlonco <marlonco@students.s19.be>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:14:49 by marlonco          #+#    #+#             */
-/*   Updated: 2024/11/04 19:44:06 by marlonco         ###   ########.fr       */
+/*   Updated: 2024/11/05 12:10:25 by marlonco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,13 @@
     cd      --> go to home directory
     cd -    --> go to previous directory
     
-    int chdir(const char *path) --> returns 0 on success / -1 on error
+    int chdir(const char *path) 
+        --> returns 0 on success / -1 on error
+        --> can directly interpret ".." as the parent dir
+    char    *getcwd(char *buf, size_t size)
+        --> buf = array where the current working dir path will be stored 
+        --> size = the size of the buf 
+        --> returns NULL on error 
 */
 
 t_env   *find_key(char *key)
@@ -37,6 +43,18 @@ t_env   *find_key(char *key)
     return (NULL);
 }
 
+void    update_envv(char *key, char *new_value)
+{
+    t_env   *var;
+    
+    var = find_key(key);
+    if (var)
+    {
+        free(var->value);
+        var->value = ft_strdup(new_value);
+    }
+}
+
 static int  only_cd()
 {
     t_env   *home_var;
@@ -46,16 +64,11 @@ static int  only_cd()
     {
         if (chdir(home_var->value) != 0)
             return(perror("cd"), 1);
+        update_envv("OLDPWD", find_key("PWD")->value);
+        update_envv("PWD", home_var->value);
     }
     else
         return(ft_putstr_fd(2, "cd: HOME not set\n"), 1);
-    return (0);
-}
-
-static int  basic_cd(char *path)
-{
-    if (chdir(path) != 0)
-        return(perror("cd"), 1);
     return (0);
 }
 
@@ -68,11 +81,35 @@ static int  minus_cd()
     {
         if(chdir(oldpwd_var) != 0)
             return(perror("cd"), 1);
+        update_envv("OLDPWD", find_key("PWD")->value);
+        update_envv("PWD", oldpwd_var->value);
     }
     else
         return(ft_putstr_fd(2, "cd: OLDPWD not set\n", 1));
     return (0);
 }
+
+// to implement : tilde & variants ; symlinks ? 
+static int  basic_cd(char *path)
+{
+    char    cwd[PATH_MAX];
+    
+    // storing the current directolry path
+    if (getcwd(cwd, sizeof(cwd)) == NULL)
+        return(perror("getcwd"), 1);
+    // changing the directory to specified path
+    if (chdir(path) != 0)
+        return(perror("cd"), 1);
+    // update oldpwd with previous path stored in cwd
+    update_envv("OLDPWD", cwd);
+    // update pwd  witgh the new cwd path
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+        update_envv("PWD", cwd);
+    else 
+        return(perror("getcwd"), 1);
+    return (0);
+}
+
 
 void    cd(char **argv)
 {
@@ -80,14 +117,12 @@ void    cd(char **argv)
 
     // IMPLEMENT ERROR HANDLING
     input = &argv[1];
-    if (list_size(input) > 2 || list_size(input) < 0)
+    if (list_size(input) > 1 || list_size(input) < 0)
         error("cd: more than 1 relative/absolute path");
-    else if (list_size(input) == 0)
+    if (list_size(input) == 0)
         only_cd();
-    else if (list_size(input) == 1)
-        basic_cd(input[0]);
-    else if (list_size(input) == 2 && ft_strncmp(input[0], "-", INT_MAX))
+    else if (ft_strncmp(input[0], "-", INT_MAX))
         minus_cd();
     else
-        return;
+        basic_cd(input[0]);
 }
