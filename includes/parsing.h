4bac3d8bc8487c6de6415785f5c9e144e78c5625
Margaux Loncour <marlonco@mi-c1-p1.s19.be>
2024-12-19 13:38:38 +0100

    norme v2

--- includes/parsing.h
+++ includes/parsing.h
@@ -13,9 +13,9 @@
 #ifndef PARSING_H
 # define PARSING_H
 
-#include <stdbool.h>
+# include <stdbool.h>
 
-enum e_char_token
+enum				e_char_token
 {
 	PIPE,
 	WORD,
@@ -26,55 +26,57 @@ enum e_char_token
 	REDIR,
 	CMD,
 };
-//env
-typedef struct s_env {
+// env
+typedef struct s_env
+{
 	char			*name;
 	char			*value;
 	struct s_env	*next;
-}	t_env;
+}					t_env;
 //--
-typedef struct	s_node
+typedef struct s_node
 {
-	int	type;
-}	t_node;
+	int				type;
+}					t_node;
 
 typedef struct s_cmd
 {
-	int		type;
-	char 	**argv;
-	t_env 	**g_env;
-}	t_cmd;
+	int				type;
+	char			**argv;
+	t_env			**g_env;
+}					t_cmd;
 
- typedef struct s_redir 
+typedef struct s_redir
 {
-	int		type;
-	t_node	*cmd;
-	char	*file;
-}	t_redir;
+	int				type;
+	t_node			*cmd;
+	char			*file;
+}					t_redir;
 
- typedef struct s_pipe
+typedef struct s_pipe
 {
-	int		type;
-	t_node	*left;
-	t_node	*right;
-}	t_pipe;
+	int				type;
+	t_node			*left;
+	t_node			*right;
+}					t_pipe;
 
-extern int rl_catch_signals;
+extern int			rl_catch_signals;
 
 typedef struct s_trim
 {
-    bool    in_single;
-    bool    in_double;
-    int     c;
-    int     end_index;
-    int     i;
-    int     j;
-	int		k;
-    int     flag;
-	char	*new_str;
-	char	*temp;
-}   t_trim;
+	bool			in_single;
+	bool			in_double;
+	int				c;
+	int				end_index;
+	int				i;
+	int				j;
+	int				k;
+	int				flag;
+	char			*new_str;
+	char			*temp;
+}					t_trim;
 
-//GLOBAL VAR
-int g_exitcode;
+// GLOBAL VAR
+int					g_exitcode;
+
 #endif
